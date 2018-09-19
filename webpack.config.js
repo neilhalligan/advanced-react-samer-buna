972e059e2b9f0aa6f1fdb7c6a1e21a0de86baffc
Neil Halligan <neilhalligan2972@gmail.com>
2018-09-19 15:48:56 +0100

    final commit

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,14 @@ module.exports = {
   },
   module: {
     rules: [
-      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['react', 'env', 'stage-2'] }
+        }
+      }
     ]
   }
 };
